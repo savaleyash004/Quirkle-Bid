d3dcd08fe8a8297bcb8edd326dd99cb66b0595e6
savaleyash004 <savaleyash004@gmail.com>
2025-07-27 20:49:44 +0530

    backend update

--- Auction-website-MERN-Stack-main/backend/src/app.js
+++ Auction-website-MERN-Stack-main/backend/src/app.js
@@ -7,7 +7,7 @@ const app=express();
 
 
 
-app.use(cors({origin:"https://quirkle-bid-3.onrender.com/",credentials:true}));
+app.use(cors({origin:"https://quirkle-bid-3.onrender.com",credentials:true}));
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
